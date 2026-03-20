8c90f729e3fac802ef7b31f6f65a8435263d92b5
Tirath957 <ktirath001@gmail.com>
2026-03-20 15:11:45 +0530

    UC1: Welcome message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,9 @@
+/**
+ * Book My Stay App
+ * Version 1.0
+ */
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App v1.0");
+    }
+}
