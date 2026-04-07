90c2268d99d17b3398867e1f4a70c429d49acbae
Tirath Kumar <ktirath001@gmail.com>
2026-04-07 14:50:20 +0530

    Update BookMyStayApp.java

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,9 +1,55 @@
-/**
- * Book My Stay App
- * Version 1.0
- */
-public class BookMyStayApp {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main
+{
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay App v1.0");
+
+        // UC1 - Welcome Message
+        System.out.println("Welcome to BookMyStayApp!");
+        System.out.println("Your simple hotel booking system\n");
+
+        // UC3 - Centralized Room Inventory
+        RoomInventory inventory = new RoomInventory();
+
+        System.out.println("Current Room Availability:");
+        inventory.displayInventory();
+    }
+}
+
+/*
+ UC3 - RoomInventory Actor
+ Responsible for centralized management of room availability
+*/
+class RoomInventory {
+
+    private HashMap<String, Integer> roomAvailability;
+
+    // Constructor initializes room inventory
+    public RoomInventory() {
+        roomAvailability = new HashMap<>();
+
+        roomAvailability.put("Single Room", 5);
+        roomAvailability.put("Double Room", 4);
+        roomAvailability.put("Deluxe Room", 3);
+        roomAvailability.put("Suite Room", 2);
+    }
+
+    // Retrieve availability
+    public int getAvailability(String roomType) {
+        return roomAvailability.getOrDefault(roomType, 0);
+    }
+
+    // Update availability
+    public void updateAvailability(String roomType, int newCount) {
+        roomAvailability.put(roomType, newCount);
+    }
+
+    // Display inventory
+    public void displayInventory() {
+        for (Map.Entry<String, Integer> entry : roomAvailability.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue() + " rooms available");
+        }
     }
 }
